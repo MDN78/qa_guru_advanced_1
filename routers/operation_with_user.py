044dfc782589dcde859a617fbaed1417bdf6d289
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2025-01-18 19:30:47 +0300

    add projects updates

--- routers/operation_with_user.py
+++ routers/operation_with_user.py
@@ -8,14 +8,25 @@ router = APIRouter(prefix='/api', tags=['operation with users'])
 now_data = datetime.datetime.now(datetime.UTC).strftime("%H:%M-%d.%m.%Y")
 
 
-@router.post("/users", response_model=UserCreatedData, status_code=status.HTTP_201_CREATED)
+# @router.post("/users", response_model=UserCreatedData, status_code=status.HTTP_201_CREATED)
+# async def create_user(name: str, job: str = 'QA') -> UserCreatedData:
+#     new_id = random.randint(99, 150)
+#     date = now_data
+#     return UserCreatedData(name=name, job=job, id=new_id, createdAt=date)
+
+
+@router.post("/users", response_model=UserUpdatedData, status_code=status.HTTP_201_CREATED)
 async def create_user(name: str, job: str = 'QA') -> UserCreatedData:
-    new_id = random.randint(99, 150)
-    date = now_data
-    return UserCreatedData(name=name, job=job, id=new_id, createdAt=date)
+    new_user = UserCreatedData(
+        name=name,
+        job=job,
+        id=random.randint(99, 150),
+        createdAt=now_data
+    )
 
+    return new_user
 
-@router.put("/users/{user_id}", response_model=UserUpdatedData)
-async def update_user(user_id: int, name: str, job: str) -> UserUpdatedData:
-    date = now_data
-    return UserUpdatedData(name=name, job=job, updatedAt=date)
+# @router.put("/users/{user_id}", response_model=UserUpdatedData)
+# async def update_user(user_id: int, name: str, job: str) -> UserUpdatedData:
+#     date = now_data
+#     return UserUpdatedData(name=name, job=job, updatedAt=date)
--- tests/test_operation_with_user.py
+++ tests/test_operation_with_user.py
@@ -6,6 +6,7 @@ def test_create_new_user():
     name = os.getenv('NEW_USER_NAME')
     job = os.getenv('NEW_USER_JOB')
     new_user = api.create_new_user(name, job)
+    print(new_user)
     status_code = new_user[1]
     assert status_code == 201
     assert new_user[0]["name"] == name
