63a229da34a6d828d8e1e1dc9d203b136d62b386
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2025-01-18 19:33:08 +0300

    add projects updates

--- routers/operation_with_user.py
+++ routers/operation_with_user.py
@@ -8,23 +8,23 @@ router = APIRouter(prefix='/api', tags=['operation with users'])
 now_data = datetime.datetime.now(datetime.UTC).strftime("%H:%M-%d.%m.%Y")
 
 
-# @router.post("/users", response_model=UserCreatedData, status_code=status.HTTP_201_CREATED)
-# async def create_user(name: str, job: str = 'QA') -> UserCreatedData:
-#     new_id = random.randint(99, 150)
-#     date = now_data
-#     return UserCreatedData(name=name, job=job, id=new_id, createdAt=date)
-
-
-@router.post("/users", response_model=UserUpdatedData, status_code=status.HTTP_201_CREATED)
+@router.post("/users", response_model=UserCreatedData, status_code=status.HTTP_201_CREATED)
 async def create_user(name: str, job: str = 'QA') -> UserCreatedData:
-    new_user = UserCreatedData(
-        name=name,
-        job=job,
-        id=random.randint(99, 150),
-        createdAt=now_data
-    )
+    new_id = random.randint(99, 150)
+    date = now_data
+    return UserCreatedData(name=name, job=job, id=new_id, createdAt=date)
 
-    return new_user
+
+# @router.post("/users", response_model=UserUpdatedData, status_code=status.HTTP_201_CREATED)
+# async def create_user(name: str, job: str = 'QA') -> UserCreatedData:
+#     new_user = UserCreatedData(
+#         name=name,
+#         job=job,
+#         id=random.randint(99, 150),
+#         createdAt=now_data
+#     )
+#
+#     return new_user
 
 # @router.put("/users/{user_id}", response_model=UserUpdatedData)
 # async def update_user(user_id: int, name: str, job: str) -> UserUpdatedData:
