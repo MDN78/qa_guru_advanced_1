84d808368a7b62d8f5707ade2b9b5c9b156ad013
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2025-01-19 21:07:14 +0300

    add projects updates

--- routers/operation_with_user.py
+++ routers/operation_with_user.py
@@ -1,4 +1,4 @@
-from fastapi import HTTPException, APIRouter, status, Body
+from fastapi import HTTPException, APIRouter, status, Body, Response
 from models.users import UserCreatedData, UserUpdatedData
 import datetime
 import random
@@ -13,6 +13,18 @@ async def create_user(name: str, job: str = 'QA') -> UserCreatedData:
     new_id = random.randint(99, 150)
     date = now_data
     return UserCreatedData(name=name, job=job, id=new_id, createdAt=date)
+
+# @router.post("/users", response_model=UserCreatedData, status_code=status.HTTP_201_CREATED)
+# async def create_user(user: UserCreatedData, name: str, job: str) -> UserCreatedData:
+#     new_id = random.randint(99, 150)
+#     date = now_data
+#     return UserCreatedData(name=name, job=job, id=new_id, createdAt=date)
+
+# @router.post("/users", response_model=UserCreatedData, status_code=status.HTTP_201_CREATED)
+# async def create_user(user: UserCreatedData) -> UserCreatedData:
+#     new_id = random.randint(99, 150)
+#     date = now_data
+#     return UserCreatedData(id=new_id, createdAt=date)
 
 
 # @router.post("/users", response_model=UserUpdatedData, status_code=status.HTTP_201_CREATED)
